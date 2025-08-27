e804925e54bfdc12288997449408c077df2d0d9c
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-08-27 23:22:49 +0500

    TopDoctors Component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,10 @@ export default {
         primary: '#5f6fff',
         secondary: '#9333EA',
         accent: '#FBBF24',
-      }
+      },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))',
+      },
     },
   },
   plugins: [],
