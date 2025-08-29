b345d7b0d47909e775f5625212b2a628d13f51d6
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-08-29 22:24:29 +0500

    another component

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,19 +1,31 @@
-import React, { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
-const TopDoctors = () => {
+const RelatedDoctors = ({ docId, speciality }) => {
 
-    const navigate = useNavigate();
     const { doctors } = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const [relDocs, setRelDocs] = useState([]);
+
+    useEffect(() => {
+        if (doctors && speciality) {
+            const related = doctors.filter(doc => doc.speciality === speciality && doc._id !== docId);
+            setRelDocs(related);
+
+        }
+
+    }, [doctors, docId, speciality]);
+
 
     return (
         <div className='flex flex-col items-center my-6 gap-4 text-gray-900 md:mx-10'>
-            <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+            <h1 className='text-3xl font-medium'>Related Doctors</h1>
             <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
             <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
                 {
-                    doctors.slice(0, 8).map((item, index) => (
+                    relDocs.slice(0, 8).map((item, index) => (
                         <div onClick={() => {navigate(`/appointment/${item._id}`); scrollTo(0,0)}} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-300'>
                             <img className='bg-blue-50' src={item.image} alt="" />
                             <div className='p-4'>
@@ -34,4 +46,4 @@ const TopDoctors = () => {
     )
 }
 
-export default TopDoctors
+export default RelatedDoctors
