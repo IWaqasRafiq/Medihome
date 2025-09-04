dbf9f12a785405a82b29d44c81b29c74b6951e66
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-09-04 23:56:57 +0500

    Add Doctor Page

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -13,19 +13,19 @@ const Sidebar = () => {
                     <ul className='text-[#515151] mt-5'>
                         <NavLink className={({ isActive }) =>  `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 ${isActive ? 'bg-[#F2F3Ff] border-r-4 border-primary' : ''}`} to={'/admin-dashboard'}>
                             <img src={assets.home_icon} alt="" />
-                            <p>Dashboard</p>
+                            <p className='sm:block hidden'>Dashboard</p>
                         </NavLink>
                         <NavLink className={({ isActive }) =>  `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 ${isActive ? 'bg-[#F2F3Ff] border-r-4 border-primary' : ''}`} to={'/doctors-list'}>
                             <img src={assets.people_icon} alt="" />
-                            <p>Doctors List</p>
+                            <p className='sm:block hidden'>Doctors List</p>
                         </NavLink>
                         <NavLink className={({ isActive }) =>  `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 ${isActive ? 'bg-[#F2F3Ff] border-r-4 border-primary' : ''}`} to={'/all-appointments'}>
                             <img src={assets.appointment_icon} alt="" />
-                            <p>All Appointments</p>
+                            <p className='sm:block hidden'>All Appointments</p>
                         </NavLink>
                         <NavLink className={({ isActive }) =>  `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 ${isActive ? 'bg-[#F2F3Ff] border-r-4 border-primary' : ''}`} to={'/add-doctor'}>
                             <img src={assets.add_icon} alt="" />
-                            <p>Add Doctor</p>
+                            <p className='sm:block hidden'>Add Doctor</p>
                         </NavLink>
                     </ul>
                 )
--- admin/src/pages/admin/AddDoctor.jsx
+++ admin/src/pages/admin/AddDoctor.jsx
@@ -1,144 +1,173 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 
 const AddDoctor = () => {
+
+    const [docImage, setDocImage] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [experience, setExperience] = useState('1 Year');
+    const [degree, setDegree] = useState('');
+    const [speciality, setSpeciality] = useState('general Physician');
+    const [fees, setFees] = useState('');
+    const [about, setAbout] = useState('');
+    const [education, setEducation] = useState('');
+    const [address1, setAddress1] = useState('');
+    const [address2, setAddress2] = useState('');
+
     return (
-        <form>
-            <p>Add Doctor</p>
-            <div>
-                <div>
+        <form className='overflow-x-scroll scroll-smooth'>
+            <p className='mt-3 sm:mt-6 mx-4 sm:mx-8  text-xl font-semibold'>Add Doctor</p>
+            <div className='flex flex-col  bg-white my-4 sm:my-8 mx-4 sm:mx-8 p-8'>
+                <div className='flex items-center gap-4 mb-8'>
                     <label htmlFor='doctor_image'>
-                        <img src={assets.upload_area} alt="" />
+                        <img src={docImage ? URL.createObjectURL(docImage) : assets.upload_area} alt="" className='cursor-pointer bg-gray-100 w-20 rounded-full border border-gray-200' />
                     </label>
-                    <input type="file" id="doctor_image" hidden />
-                    <p>Upload Doctor <br /> Profile</p>
+                    <input onChange={(e)=> setDocImage(e.target.files[0])} type="file" id="doctor_image" hidden />
+                    <p className='font-mono text-gray-600'>Upload Doctor <br /> Profile</p>
                 </div>
                 <div>
                     {/* Doctor Info */}
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Doctor name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                // value={formData.name}
-                                // onChange={handleChange}
-                                placeholder="Name"
-                                className="w-full border rounded-md px-3 py-2 text-sm  focus:ring focus:ring-blue-300"
-                            />
+                        <div className='gap-4 flex flex-col'>
+                            <div>
+                                <label className="block text-md text-gray-500 font-medium mb-1">Doctor name</label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder="Name"
+                                    className="w-full border rounded-md px-3 py-2 text-sm outline-none"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-md text-gray-500 font-medium mb-1">Doctor Email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="Your email"
+                                    className="w-full border rounded-md px-3 py-2 text-sm outline-none"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-md text-gray-500 font-medium mb-1">Doctor Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={password}
+                                    onChange={(e)=> setPassword(e.target.value)}
+                                    placeholder="Password"
+                                    className="w-full border rounded-md px-3 py-2 text-sm outline-none"
+                                />
+                            </div>
+                            <div>
+                                <label className="block text-md text-gray-500 font-medium mb-1">Experience</label>
+                                <select
+                                    name="experience"
+                                    // value={formData.experience}
+                                    // onChange={handleChange}
+                                    className="w-full border rounded-md text-gray-500  px-3 py-2 text-sm outline-none"
+                                >
+                                    <option className='text-gray-600' value="1 year">1 Year</option>
+                                    <option className='text-gray-600' value="2 years">2 Years</option>
+                                    <option className='text-gray-600' value="2 years">3 Years</option>
+                                    <option className='text-gray-600' value="2 years">4 Years</option>
+                                    <option className='text-gray-600' value="2 years">5 Years</option>
+                                    <option className='text-gray-600' value="5 years">5 Years</option>
+                                    <option className='text-gray-600' value="5 years">5+ Years</option>
+                                    <option className='text-gray-600' value="5 years">8+ Years</option>
+                                    <option className='text-gray-600' value="10+ years">10+ Years</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label className="block text-md text-gray-500 font-medium mb-1">Fees</label>
+                                <input
+                                    type="number"
+                                    name="fees"
+                                    // value={formData.fees}
+                                    // onChange={handleChange}
+                                    placeholder="Your fees"
+                                    className="w-full border rounded-md px-3 py-2 text-sm outline-none"
+                                />
+                            </div>
                         </div>
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Speciality</label>
-                            <select
-                                name="speciality"
-                                // value={formData.speciality}
-                                // onChange={handleChange}
-                                className="w-full border rounded-md px-3 py-2 text-sm focus:ring focus:ring-blue-300"
-                            >
-                                <option>General physician</option>
-                                <option>Cardiologist</option>
-                                <option>Dermatologist</option>
-                                <option>Neurologist</option>
-                            </select>
+                        <div className='gap-4 flex flex-col'>
+
+                            <div>
+                                <label className="block text-md text-gray-600 font-medium mb-1">Speciality</label>
+                                <select
+                                    name="speciality"
+                                    // value={formData.speciality}
+                                    // onChange={handleChange}
+                                    className="w-full border rounded-md px-3 text-gray-500 py-2 text-sm outline-none"
+                                >
+                                    <option className='text-gray-600'>General physician</option>
+                                    <option className='text-gray-600'>Cardiologist</option>
+                                    <option className='text-gray-600'>Dermatologist</option>
+                                    <option className='text-gray-600'>Gynecologist</option>
+                                    <option className='text-gray-600'>Neurologist</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <label className="block text-md text-gray-500 font-medium mb-1">Education</label>
+                                <input
+                                    type="text"
+                                    name="education"
+                                    // value={formData.education}
+                                    // onChange={handleChange}
+                                    placeholder="Education"
+                                    className="w-full border rounded-md px-3 py-2 text-sm outline-none"
+                                />
+                            </div>
+
+                            <div>
+                                <label className="block text-md text-gray-500 font-medium mb-1">Address</label>
+                                <input
+                                    type="text"
+                                    name="address1"
+                                    // value={formData.address1}
+                                    // onChange={handleChange}
+                                    placeholder="Address 1"
+                                    className="w-full border rounded-md px-3 py-2 text-sm mb-2 outline-none"
+                                />
+                                <input
+                                    type="text"
+                                    name="address2"
+                                    // value={formData.address2}
+                                    // onChange={handleChange}
+                                    placeholder="Address 2"
+                                    className="w-full border rounded-md px-3 py-2 text-sm outline-none"
+                                />
+                            </div>
                         </div>
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Doctor Email</label>
-                            <input
-                                type="email"
-                                name="email"
-                                // value={formData.email}
-                                // onChange={handleChange}
-                                placeholder="Your email"
-                                className="w-full border rounded-md px-3 py-2 text-sm focus:ring focus:ring-blue-300"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Education</label>
-                            <input
-                                type="text"
-                                name="education"
-                                // value={formData.education}
-                                // onChange={handleChange}
-                                placeholder="Education"
-                                className="w-full border rounded-md px-3 py-2 text-sm focus:ring focus:ring-blue-300"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Doctor Password</label>
-                            <input
-                                type="password"
-                                name="password"
-                                // value={formData.password}
-                                // onChange={handleChange}
-                                placeholder="Password"
-                                className="w-full border rounded-md px-3 py-2 text-sm focus:ring focus:ring-blue-300"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Address</label>
-                            <input
-                                type="text"
-                                name="address1"
-                                // value={formData.address1}
-                                // onChange={handleChange}
-                                placeholder="Address 1"
-                                className="w-full border rounded-md px-3 py-2 text-sm mb-2 focus:ring focus:ring-blue-300"
-                            />
-                            <input
-                                type="text"
-                                name="address2"
-                                // value={formData.address2}
-                                // onChange={handleChange}
-                                placeholder="Address 2"
-                                className="w-full border rounded-md px-3 py-2 text-sm focus:ring focus:ring-blue-300"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Experience</label>
-                            <select
-                                name="experience"
-                                // value={formData.experience}
-                                // onChange={handleChange}
-                                className="w-full border rounded-md  px-3 py-2 text-sm focus:ring focus:ring-blue-300"
-                            >
-                                <option value="">Experience</option>
-                                <option value="1 year">1 Year</option>
-                                <option value="2 years">2 Years</option>
-                                <option value="5 years">5 Years</option>
-                                <option value="10+ years">10+ Years</option>
-                            </select>
-                        </div>
+
                         <br />
-                        <div>
-                            <label className="block text-sm font-medium mb-1">Fees</label>
-                            <input
-                                type="number"
-                                name="fees"
-                                // value={formData.fees}
-                                // onChange={handleChange}
-                                placeholder="Your fees"
-                                className="w-full border rounded-md px-3 py-2 text-sm focus:ring focus:ring-blue-300"
-                            />
-                        </div>
+
                     </div>
 
                     {/* About */}
                     <div>
-                        <label className="block text-sm font-medium mb-1">About me</label>
+                        <label className="block text-md text-gray-500 font-medium mb-1">About me</label>
                         <textarea
                             name="about"
                             // value={formData.about}
                             // onChange={handleChange}
                             placeholder="Write about yourself"
                             rows="4"
-                            className="w-full border rounded-md px-3 py-2 text-sm focus:ring focus:ring-blue-300"
+
+                            className="w-full border rounded-md px-3 py-2 text-sm outline-none"
                         />
                     </div>
 
                     {/* Submit Button */}
                     <button
                         type="submit"
-                        className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md text-sm"
+                        className="bg-blue-500 hover:bg-blue-600 mt-3 text-white px-6 py-2 rounded-md text-sm"
                     >
                         Add doctor
                     </button>
